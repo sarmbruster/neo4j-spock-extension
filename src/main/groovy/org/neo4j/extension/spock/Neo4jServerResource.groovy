740edce1ff4dc303580877ff59bcc7c4c06b40c6
Stefan Armbruster <stefan@armbruster-it.de>
2013-12-15 19:55:52 +0100

    refactor server tests to use a rule instead of base class

---
+++ src/main/groovy/org/neo4j/extension/spock/Neo4jServerResource.groovy
@@ -0,0 +1,58 @@
+package org.neo4j.extension.spock
+
+import org.junit.rules.ExternalResource
+import org.neo4j.cypher.javacompat.ExecutionEngine
+import org.neo4j.graphdb.GraphDatabaseService
+import org.neo4j.server.NeoServer
+
+/**
+ * abstract base class for spock tests using a Neo4j server
+ */
+class Neo4jServerResource extends ExternalResource {
+
+    GraphDatabaseService graphDatabaseService
+    NeoServer server
+    int port
+    def thirdPartyJaxRsPackages = [:]
+    def config = [:]
+    String baseUrl
+    @Lazy
+    ExecutionEngine executionEngine = new ExecutionEngine(graphDatabaseService)
+
+    @Override
+    protected void before() throws Throwable {
+        port = findFreePort()
+        def serverBuilder = new ConfigurableServerBuilder()
+
+        thirdPartyJaxRsPackages.each { packageName, mountPoint ->
+            serverBuilder.withThirdPartyJaxRsPackage(packageName, mountPoint)
+        }
+        config.each { k, v ->
+            serverBuilder.withConfigProperty(k, v)
+        }
+
+        server = serverBuilder.onPort(port).build();
+        server.start()
+        baseUrl = server.baseUri().toASCIIString()
+        graphDatabaseService = server.database.graph
+        String.metaClass.cypher = { -> executionEngine.execute(delegate) }
+        String.metaClass.cypher = { Map params -> executionEngine.execute(delegate, params) }
+    }
+
+    @Override
+    protected void after() {
+        server.stop()
+    }
+
+    int findFreePort() {
+        ServerSocket server = new ServerSocket(0)
+        int port = server.localPort
+        server.close()
+        port
+    }
+
+    def withTransaction(Closure closure) {
+        Neo4jUtils.withSuccessTransaction(graphDatabaseService, closure)
+    }
+
+}
---
+++ src/test/groovy/org/neo4j/extension/spock/SampleNeo4jServerSpec.groovy
@@ -0,0 +1,100 @@
+package org.neo4j.extension.spock
+
+import org.junit.ClassRule
+import org.neo4j.helpers.collection.Iterables
+import org.neo4j.kernel.guard.Guard
+import org.neo4j.test.server.HTTP
+import org.neo4j.tooling.GlobalGraphOperations
+import spock.lang.Shared
+import spock.lang.Specification
+
+class SampleNeo4jServerSpec extends Specification {
+
+    /**
+     * we need to define our test resources: a neo4j server
+     * two options:
+     * <ol>
+     *     <li>one server per test class: use @Shared and @ClassRule</li>
+     *     <li>one server per test method: use @Rule and optionally @Delegate
+     * </ol>
+     * N.B. @Delegate and @Shared cannot be combined!
+     */
+    @Shared
+    @ClassRule
+    Neo4jServerResource neo4j = new Neo4jServerResource(
+            config: [execution_guard_enabled: "true"]
+    )
+
+    def "server is running"() {
+        expect:
+        neo4j.baseUrl == "http://localhost:${neo4j.port}/"
+        HTTP.GET(neo4j.baseUrl).status() == 200
+    }
+
+    def "standard cypher endpoint is working"() {
+        setup:
+        def nodeCount = getNodeCount()
+
+        when:
+        def json = [
+                query: "match (n) return count(n)"
+        ]
+
+        def response = HTTP.POST("${neo4j.baseUrl}db/data/cypher", json)
+
+        then:
+        response.status() == 200
+        response.content().data.size() == 1
+        response.content().data[0][0] == nodeCount
+
+    }
+
+    def "transactional cypher endpoint is working"() {
+        setup:
+        def nodeCount = getNodeCount()
+
+        when:
+        def json = [
+                statements: [
+                    [
+                            statement: "match (n) return count(n)",
+                            parameters: [:]
+                    ]
+                ]
+        ]
+        def response = HTTP.POST("${neo4j.baseUrl}db/data/transaction", json)
+
+        then:
+        response.status() == 201
+        response.content().commit =~ "http://localhost:\\d+/db/data/transaction/1/commit"
+        response.content().results.size() == 1
+        response.content().results[0].columns[0] == "count(n)"
+        response.content().results[0].data.size() == 1
+        response.content().results[0].data.row[0][0] == nodeCount
+
+    }
+
+    def "cypher method on String is working"() {
+        when:
+        def result = "match (n) return count(n) as count".cypher()
+
+        then:
+        result[0].count instanceof Number
+    }
+
+
+    def "guard is enabled in this specification"() {
+        when:
+        neo4j.server.database.graph.dependencyResolver.resolveDependency(Guard)
+
+        then:
+        notThrown IllegalArgumentException
+    }
+
+    private def getNodeCount() {
+        neo4j.withTransaction {
+            Iterables.count(GlobalGraphOperations.at(neo4j.graphDatabaseService).allNodes)
+        }
+    }
+
+}
