9a3758876fbc2c7a5450d25a657e496fc254e067
Stefan Armbruster <stefan@armbruster-it.de>
2013-12-15 19:53:55 +0100

    remove license headers - we have LICENSE.txt

--- src/main/groovy/org/neo4j/extension/spock/Neo4jTransactionExtension.groovy
+++ src/main/groovy/org/neo4j/extension/spock/Neo4jTransactionExtension.groovy
@@ -1,19 +1,3 @@
-/*
- * Copyright 2013 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package org.neo4j.extension.spock
 
 import org.spockframework.runtime.extension.AbstractAnnotationDrivenExtension
@@ -21,6 +5,10 @@ import org.spockframework.runtime.extension.AbstractMethodInterceptor
 import org.spockframework.runtime.extension.IMethodInvocation
 import org.spockframework.runtime.model.FeatureInfo
 
+/**
+ * provide transactional context when test method is annotated with {@link WithNeo4jTransaction}
+ * as a requirement, the test class needs to have a field of type {@link Neo4jResource}
+ */
 class Neo4jTransactionExtension extends AbstractAnnotationDrivenExtension<WithNeo4jTransaction> {
 
     @Override
@@ -28,7 +16,6 @@ class Neo4jTransactionExtension extends AbstractAnnotationDrivenExtension<WithNe
         feature.featureMethod.addInterceptor(new AbstractMethodInterceptor() {
             @Override
             void interceptFeatureMethod(IMethodInvocation invocation) throws Throwable {
-
                 Neo4jResource value = invocation.instance.properties.values().find { it instanceof Neo4jResource}
                 assert value, "no Neo4jResource defined in specification ${invocation.instance.class.name}"
                 Neo4jUtils.withSuccessTransaction(value.graphDatabaseService, { invocation.proceed() } )
